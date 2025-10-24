d035af6c7cce1e317c186a621c172b0ee9355af4
Gadiel Rangel <ig-grangel@totalwine.com>
2025-10-24 16:25:02 -0300

    Format code with rubocop

--- db/migrate/20251023112713_add_unique_index_to_menu_items.rb
+++ db/migrate/20251023112713_add_unique_index_to_menu_items.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToMenuItems < ActiveRecord::Migration[8.0]
   def change
-    add_index :menu_items, [:name, :price], unique: true
+    add_index :menu_items, [ :name, :price ], unique: true
   end
 end
